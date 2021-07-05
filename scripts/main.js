3772458a60c634e1c4957d6f204cd2bfaf6110dd
ToriToki <toritokidev@protonmail.com>
2021-07-05 10:40:32 -0230

    Fixed image

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
         let mySrc = myImage.getAttribute('src');
         if(mySrc === 'images/macbook.jpg') {
-            myImage.setAttribute('src','images/Toritoki.png');
+            myImage.setAttribute('src','images/developer.jpg');
         } else {
             myImage.setAttribute('src','images/macbook.jpg');
         }
